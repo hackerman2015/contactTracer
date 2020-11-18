4d67d4a7d7956ef1e3f8c40ab1a0d478e20f5948
HackermanResurrected <61045703+hackerman2015@users.noreply.github.com>
2020-11-17 20:10:16 -0600

    Add files via upload

---
+++ contactTracer_Reader.py
@@ -0,0 +1,40 @@
+import os
+def userInput():
+    print('Querying for a location and time will return all users at\nthat location at the specified time.')
+    print('******************************************************')
+    location = input('What location are you querying for? ')
+    time = input('What time are you querying for? ')
+
+    return location, time
+
+
+def reader(query):
+    tempList = []
+    ## If statements for location + time query
+##    directory = input('Type the directory the user files are in: ')
+    location = query[0]
+    time = query[1]
+    directory = '/probatorem/pyProjects/FinalProject/'
+    for filename in os.listdir(directory):
+        if '.csv' in filename:
+            file = open(directory + filename, 'r')
+            for x in file:
+                tempList = x.split(',')
+                for t in tempList:
+                    print(t)
+##                    if time in t:
+##                        print('yeet')
+    ## Different if statements depending on length of query
+
+
+def main():
+##    query = userInput()
+    query = ('location', '4AM - 8AM')
+    reader(query)
+    
+    
+
+
+
+
+main()
---
+++ contactTracing_Writer.py
@@ -0,0 +1,44 @@
+import csv
+def userInput():
+    userData = []
+    tempList = []
+    userDict = {}
+    
+    i = 0
+    x = ''
+    userName = input('Type your name: ')
+    while x != 'Quit':
+        tempList = []
+        x = input('Please provide all locations you have been.\nAfter you write one location, press enter.\nDo this until all locations have been entered.\nLocation: ')
+        if x == 'q' or x == 'Quit' or x == 'quit':
+            break
+        t = input('Now provide the time you were at this location.\nFormat = "6AM - 8AM"\nTime: ')
+        tempList.append(x)
+        tempList.append(t)
+        userData.append(tempList)
+                    
+        print('If you have finished, type "Quit".')
+    userDict[userName] = userData
+    return userDict
+
+def dataCompiler(userDict):
+    for x in userDict:
+        fileName = x + '.csv'
+
+    file = open(fileName, 'w')
+    for x, y in userDict.items():
+        file = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        file.writerow([x, y])
+
+    
+
+def main():
+##    userDict = userInput()
+    userDict = {'steve': [['location', '4AM - 8AM'], ['mexico', '10PM - 11PM']]}
+    dataCompiler(userDict)
+    
+    
+
+
+main()
